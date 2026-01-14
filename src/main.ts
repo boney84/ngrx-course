397284bba36b0eae4bec34ed422f30c2590b1df4
boney <boney0084@gmail.com>
2026-01-14 18:46:49 +0530

    packages updated

--- src/main.ts
+++ src/main.ts
@@ -10,5 +10,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
